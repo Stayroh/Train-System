bbe3abd0a8f162d249888302325bfec590b02172
Stayroh <jakoballweiss@outlook.de>
2022-11-08 00:27:17 +0100

    a

--- src/shared/Math.lua
+++ src/shared/Math.lua
@@ -1,16 +1,20 @@
 local Math = {}
 
-function Math.RotateOverVector(V1: Vector3, V2: Vector3, Angel: number): Vector3
+function Math.RotateOverVector(V1: Vector3, V2: Vector3, Angle: number): Vector3
 	local GC = V1:Cross(V2).Unit
-	return V1 * math.cos(Angel) + GC:Cross(V1) * math.sin(Angel)
+	return V1 * math.cos(Angle) + GC:Cross(V1) * math.sin(Angle)
 end
 
 function Math.ArcLatitudeIntersection(V1: Vector3, V2: Vector3, Height: number): number
 	local GC = V1:Cross(V2).Unit
 	local a = V1.Z
-	local b = V1.Y * GC.X - V1.X * GC.Y
+	local b = V1.X * GC.Y - V1.Y * GC.X
 	local c = Height
-	return math.atan(b / a) - math.acos(c / math.sqrt(a ^ 2 + b ^ 2))
+	local atan = math.atan(b / a)
+	local acos = math.acos(c / math.sqrt(a ^ 2 + b ^ 2))
+	local Alpha = atan - acos
+	print(a, b, atan, acos, Alpha)
+	return Alpha
 end
 
 return Math
