7d2e2345172009c7d93d335c896fe812f7a8ac49
Stayroh <jakoballweiss@outlook.de>
2022-11-07 19:40:24 +0100

    a

--- src/shared/Math.lua
+++ src/shared/Math.lua
@@ -5,4 +5,12 @@ function Math.RotateOverVector(V1: Vector3, V2: Vector3, Angel: number): Vector3
 	return V1 * math.cos(Angel) + GC:Cross(V1) * math.sin(Angel)
 end
 
+function Math.ArcLatitudeIntersection(V1: Vector3, V2: Vector3, Height: number): number
+	local GC = V1:Cross(V2).Unit
+	local a = V1.Z
+	local b = V1.Y * GC.X - V1.X * GC.Y
+	local c = Height
+	return math.atan(b / a) - math.acos(c / math.sqrt(a ^ 2 + b ^ 2))
+end
+
 return Math
--- src/shared/Test.lua
+++ src/shared/Test.lua
@@ -1,6 +1,6 @@
 local Module = {}
 local Math = require(game.ReplicatedStorage.source.Math)
-function Module.Test(Angel)
+function Module.Test(Height)
 	local S = game.Workspace.Sphere
 	local A = workspace.PointA
 	local B = workspace.PointB
@@ -8,6 +8,7 @@ function Module.Test(Angel)
 	local Radius = S.Size.X / 2
 	local V1 = (A.Position - S.Position).Unit
 	local V2 = (B.Position - S.Position).Unit
-	R.Position = (Math.RotateOverVector(V1, V2, Angel).Unit * Radius) + S.Position
+	R.Position = (Math.RotateOverVector(V1, V2, Math.ArcLatitudeIntersection(V1, V2, Height)).Unit * Radius)
+		+ S.Position
 end
 return Module
