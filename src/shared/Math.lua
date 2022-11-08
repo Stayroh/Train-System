8672507a92811276cd773003e36ca75ac64140f0
Stayroh <jakoballweiss@outlook.de>
2022-11-09 00:06:36 +0100

    a

--- src/shared/Math.lua
+++ src/shared/Math.lua
@@ -5,16 +5,20 @@ function Math.RotateOverVector(V1: Vector3, V2: Vector3, Angle: number): Vector3
 	return V1 * math.cos(Angle) + GC:Cross(V1) * math.sin(Angle)
 end
 
-function Math.ArcLatitudeIntersection(V1: Vector3, V2: Vector3, Height: number): number
+function Math.ArcLatitudeIntersection(V1: Vector3, V2: Vector3, Height: number): number?
 	local GC = V1:Cross(V2).Unit
-	local a = V1.Z
-	local b = V1.X * GC.Y - V1.Y * GC.X
+	local a = V1.Y
+	local b = GC:Cross(V1).Unit.Y
 	local c = Height
 	local atan = math.atan(b / a)
-	local acos = math.acos(c / math.sqrt(a ^ 2 + b ^ 2))
-	local Alpha = atan - acos
-	print(a, b, atan, acos, Alpha)
-	return Alpha
+	local ToAcos = c / math.sqrt(a ^ 2 + b ^ 2)
+	if ToAcos > 1 or ToAcos < -1 then
+		return nil
+	end
+	local acos = math.acos(ToAcos)
+	local Angle = atan - acos
+	print(atan, acos)
+	return Angle
 end
 
 return Math
--- src/shared/Test.lua
+++ src/shared/Test.lua
@@ -5,10 +5,16 @@ function Module.Test(Height)
 	local A = workspace.PointA
 	local B = workspace.PointB
 	local R = workspace.Result
+	local P = workspace.Plane
 	local Radius = S.Size.X / 2
+	P.Position = Vector3.new(0, Radius * Height, 0) + S.Position
 	local V1 = (A.Position - S.Position).Unit
 	local V2 = (B.Position - S.Position).Unit
 	local Angle = Math.ArcLatitudeIntersection(V1, V2, Height)
+	if not Angle then
+		print("Not succeded")
+		return
+	end
 	R.Position = Math.RotateOverVector(V1, V2, Angle).Unit * Radius + S.Position
 end
 return Module
