2cac6e41c2e4400a357f44d5de817615128d9db9
Stayroh <jakoballweiss@outlook.de>
2022-12-07 15:28:47 +0100

    aa

--- src/shared/Test.lua
+++ src/shared/Test.lua
@@ -50,7 +50,7 @@ function Module.Gamma()
 end
 
 function Module.Delta()
-	for i = 1, 2000, 1 do
+	for i = 1, 10000, 1 do
 		Math.ArcSphereIntersection(
 			Vector3.new(-1.655, 4.75, 4.187),
 			Vector3.new(-28.121, 2.575, 4.289),
