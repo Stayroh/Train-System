9a643c4293b436893a2b962f248e878ef49ebd5f
Stayroh <jakoballweiss@outlook.de>
2022-12-09 17:08:02 +0100

    a

--- src/shared/Math.lua
+++ src/shared/Math.lua
@@ -60,12 +60,6 @@ function Math.ArcLatitudeIntersection(V1: Vector3, V2: Vector3, Height: number):
 	return Angle
 end
 
-function Math.GetTangent(RelativNext: Vector3, Tangent: Vector3): Vector3
-	local d = -Tangent
-	local Normal = RelativNext.Unit
-	return d - 2 * d:Dot(Normal) * Normal
-end
-
 function Math.SphereSphereIntersection(
 	Sphere1_Pos: Vector3,
 	Sphere2_Pos: Vector3,
@@ -77,15 +71,14 @@ function Math.SphereSphereIntersection(
 end
 
 function Math.SphereFromArc(Arc_P1: Vector3, Arc_P2: Vector3, Tangent_V1: Vector3)
-	Arc_P2 -= Arc_P1
-	local Tangent_V2 = Math.GetTangent(Arc_P2, Tangent_V1)
-	local N_V1 = Tangent_V1:Cross(Arc_P2):Cross(Tangent_V1).Unit
-	local N_V2 = Tangent_V2:Cross(-Arc_P2):Cross(Tangent_V2).Unit
-	local Alpha = math.acos(N_V1:Dot(Arc_P2.Unit))
-	local Beta = math.acos(N_V2:Dot(-Arc_P2.Unit))
-	local Gamma = math.pi - (Alpha + Beta)
-	local d = Arc_P2.Magnitude / math.sin(Gamma) * math.sin(Beta)
-	return N_V1 * d + Arc_P1, d
+	local RelativP2 = Arc_P2 - Arc_P1
+	local Side = Tangent_V1:Cross(RelativP2):Cross(Tangent_V1).Unit
+	local X = Side:Dot(RelativP2)
+	local Y = Tangent_V1:Dot(RelativP2)
+	print(X, Y)
+	local H = X / 2 + (Y ^ 2) / (2 * X)
+	local Center = Side * H + Arc_P1
+	return Center, H
 end
 
 function Math.ArcSphereIntersection(
