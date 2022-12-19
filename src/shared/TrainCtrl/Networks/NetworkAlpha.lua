c191607fbcd1c0db16b831eaee497d4d270a9290
Stayroh <jakoballweiss@outlook.de>
2022-12-19 21:49:43 +0100

    Added TrainNetworkInit

---
+++ src/shared/TrainCtrl/Networks/NetworkAlpha.lua
@@ -0,0 +1,14 @@
+local Module = {
+	[1] = {
+		["Position"] = Vector3.one,
+		["Tangent"] = Vector3.new(1, 0, 0.5),
+		["UpVector"] = Vector3.new(0, 1, 0),
+	},
+	[2] = {
+		["Position"] = Vector3.one,
+		["Tangent"] = Vector3.new(1, -1, 0.1),
+		["UpVector"] = Vector3.new(0.5, 1, 0),
+	},
+}
+
+return Module
