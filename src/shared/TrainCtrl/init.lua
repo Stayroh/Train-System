a511dc3a33e3bc1c20c25c27c99e5321a2d3c94d
Stayroh <jakoballweiss@outlook.de>
2022-12-29 23:27:37 +0100

    a

--- src/shared/TrainCtrl/init.lua
+++ src/shared/TrainCtrl/init.lua
@@ -1,15 +1,29 @@
-local Knit = require(game:GetService("ReplicatedStorage").Packages.knit)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Knit = require(ReplicatedStorage.Packages.knit)
+local SwitchModule = require(script.Switch)
+local NetworkModule = require(script.Networks)
 
 local TrainController = Knit.CreateController({
 	Name = "TrainController",
 })
 
-function TrainController.KnitInit()
-	print("TrainController Initialized")
+function TrainController:KnitInit() end
+
+function TrainController:KnitStart()
+	--[[
+	self.TrainService = Knit.GetService("TrainService")
+	self.TrainService:RequestSwitchStates():andThen(function(Update)
+		print(type(Update))
+		SwitchModule:Update(Update)
+		self.TrainService.OnSwitchUpdates:Connect(function(update)
+			SwitchModule:Update(update)
+		end)
+	end)
+	]]
 end
 
-function TrainController.KnitStart()
-	print("TrainController Started")
+function TrainController:Test()
+	print(self, self == TrainController)
 end
 
 return TrainController
