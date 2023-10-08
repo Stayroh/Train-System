fb9ab66c881e52b262a051d9936c40430a665c2c
Stayroh <111523944+Stayroh@users.noreply.github.com>
2023-10-08 16:55:10 +0200

    modified train controller

--- src/shared/Controllers/TrainController.lua
+++ src/shared/Controllers/TrainController.lua
@@ -2,6 +2,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Knit = require(ReplicatedStorage.Packages.knit)
 local SwitchModule = require(ReplicatedStorage.src.TrainSystem.Switch)
 local RunService = game:GetService("RunService")
+local TrainClass = require(game.ReplicatedStorage.src.TrainSystem.TrainClass)
 
 local TrainController = Knit.CreateController({
 	Name = "TrainController",
@@ -17,7 +18,15 @@ end
 
 function TrainController:Stepped(time, deltaTime)
 	for TrainId, Train in pairs(self.Trains) do
+		Train:Step(deltaTime)
 	end
+end
+
+function TrainController:CreateTrain(Description: Types.TrainDescription)
+	if self.Trains[CarDescription.Id] then
+		warn("An already existing train was overridden")
+	end
+	self.Trains[Description.Id] = TrainClass.fromDescription(Description)
 end
 
 function TrainController:KnitInit()
